dfa1b2ae1a1b6b0c31a4e30738c73c47e69b00cb
Ajmal Afif Jamal <ajmalafif_binjamal@aviva-asia.com>
2018-05-03 16:18:56 +0800

    Tachyons and Inter UI

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import '../fonts/Inter-UI-Regular.woff2'
+import '../fonts/Inter-UI-Regular.woff'
+import '../css/font-face.css'
+import '../css/index.css'
+
+export default () => <div className="f1 pa4 link dim">Hello world!</div>
