9c9a9b6f06be9010a5553e611fa03c3c448fbca6
Ajmal Afif <ajmalafif@gmail.com>
2018-05-05 00:48:53 +0800

    Include tachyon manually

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import React from "react"
 import '../fonts/Inter-UI-Regular.woff2'
 import '../fonts/Inter-UI-Regular.woff'
 import '../css/font-face.css'
-import '../css/index.css'
+import '../index.css'
 
 export default () =>
   <div className="pa4 link dim">
-    <p className="headline gray">Hello world!</p>
+    <p className="headline gray f2">Hello world!</p>
   </div>
