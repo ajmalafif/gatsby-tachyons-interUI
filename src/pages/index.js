0e4add03cc3bc74eb9c108f254008ae07d8b82b8
Ajmal Afif <ajmalafif@gmail.com>
2018-05-10 00:31:03 +0800

    Explore Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
 import React from "react"
+import Helmet from "react-helmet";
+
 import '../fonts/Inter-UI-Regular.woff2'
 import '../fonts/Inter-UI-Regular.woff'
 import '../css/font-face.css'
 import '../css/main.css'
 
-export default () =>
-  <div className="pa4 link dim">
-    <p className="headline gray f2">Hello world!</p>
-  </div>
+const TemplateWrapper = () => (
+  <Helmet
+    title="Gatsby Tachyons InterUI"
+    meta={[
+      { name: "description", content: "Attempting Gatsby Tachyons InterUI with PWA support" },
+    ]}
+  >
+    <html lang="en" />
+  </Helmet>
+);
+
+export default TemplateWrapper;
+
+// export default () =>
+//   <div className="pa4 link dim">
+//     <p className="headline gray f2">Hello world!</p>
+//   </div>
