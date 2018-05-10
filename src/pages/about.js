0264ada2b26b219a4461be49b0afab902fb1ae4b
Ajmal Afif <ajmalafif@gmail.com>
2018-05-11 00:21:00 +0800

    About us

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../fonts/Inter-UI-Regular.woff2'
+import '../fonts/Inter-UI-Regular.woff'
+import '../css/font-face.css'
+import '../css/main.css'
+
+export default () =>
+<div className='pa4 link dim'>
+  <p className='headline gray f2'>
+    About us
+  </p>
+</div>
