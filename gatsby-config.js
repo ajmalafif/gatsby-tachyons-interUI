0308503bdb7142a159544696f6c96a6952b63942
Ajmal Afif <ajmalafif@gmail.com>
2018-05-12 02:05:02 +0800

    Add gatsby plugin netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,15 @@
-plugins: [`gatsby-plugin-react-helmet`]
+module.exports = {
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/static/*": [
+            "Cache-Control: public, max-age=16070400"
+          ]
+        }
+      }
+    }
+  ]
+};
