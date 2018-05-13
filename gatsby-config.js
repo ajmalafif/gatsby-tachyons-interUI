8a968fc1de2eef4c018fb09e35ef55b10f698ac2
Ajmal Afif <ajmalafif@gmail.com>
2018-05-14 01:05:02 +0800

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,33 @@
 module.exports = {
   plugins: [
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: false,
+          appleIcon: true,
+          appleStartup: false,
+          coast: true,
+          favicons: true,
+          firefox: false,
+          twitter: false,
+          yandex: false,
+          windows: true
+        }
+      }
+    },
     {
       resolve: `gatsby-plugin-netlify`,
       options: {
         headers: {
-          "/static/*": [
-            "Cache-Control: public, max-age=16070400"
+          '/static/*': [
+            'Cache-Control: public, max-age=16070400'
           ]
         }
       }
     }
   ]
-};
+}
