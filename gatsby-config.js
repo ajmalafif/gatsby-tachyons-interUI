e893e266a08d8422bd3fcc9944e6f74fc184e8a4
Ajmal Afif <ajmalafif@gmail.com>
2018-05-19 20:18:53 +0800

    Add manifest and offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,19 @@ module.exports = {
           ]
         }
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'ajmalafif',
+        short_name: 'ajmalafif',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#05f',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      }
+    },
+    'gatsby-plugin-offline'
   ]
 }
